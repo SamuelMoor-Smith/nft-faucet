6f71a5b545a6cf2fbaa861b413d2d8472d1d4d0a
Samuel Moor-Smith <samuelmoor-smith@Samuels-MacBook-Pro.local>
2023-06-30 21:29:55 -0700

    update layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -32,8 +32,28 @@ function App({ Component, pageProps }: AppProps) {
 
   return (
     <ChakraProvider>
-      <GlobalNav state={state} setState={setState} contractManager={contractManager} setContractManager={setContractManager} />
-      <Home state={state} setState={setState} contractManager={contractManager} setContractManager={setContractManager} />
+       <div>
+        <div className="bg-gray-1100 overflow-y-scroll pb-36">
+          <GlobalNav state={state} setState={setState} contractManager={contractManager} setContractManager={setContractManager} />
+
+          <div className="lg:pl-72">
+            <div className="mx-auto max-w-4xl space-y-8 px-2 pt-20 lg:py-8 lg:px-8">
+              {/* <div className="rounded-lg bg-vc-border-gradient p-px shadow-lg shadow-black/20">
+                <div className="rounded-lg bg-black">
+                  <AddressBar />
+                </div>
+              </div> */}
+
+              <div className="rounded-lg p-px">
+                <div className="rounded-lg p-3.5 lg:p-6">
+                  <Home state={state} setState={setState} contractManager={contractManager} setContractManager={setContractManager} />
+                </div>
+              </div>
+              {/* <Byline className="fixed sm:hidden" /> */}
+            </div>
+          </div>
+        </div>
+      </div>
     </ChakraProvider>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
-import { Layout, Text, Page } from '@vercel/examples-ui'
+import { Text, Page } from '@vercel/examples-ui'
 import { Web3Page, Web3PageProps } from '../components/Web3Page'
-import { GlobalNav } from '../components/nav/global-nav'
-import { ContractManager } from '../utils/contractManager';
-import { useState } from 'react';
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -11,16 +8,16 @@ export const Home: React.VFC<Web3PageProps> = ({ state, setState, contractManage
   return (
     <Page>
       {/* <GlobalNav state={state} setState={setState} /> */}
-      <section className="flex flex-col gap-6 ml-48">
+      <section className="flex flex-col gap-6">
         <Text variant="h1">NFT Faucet</Text>
         <Text>
           This faucet allows the user to mint NFTs from the GoerliETH testnet.
         </Text>
       </section>
 
-      <hr className="border-t border-accents-2 my-2 ml-48" />
+      <hr className="border-t border-accents-2 my-2" />
 
-      <section className="flex flex-col ml-48">
+      <section className="flex flex-col">
         <Web3Page state={state} setState={setState} contractManager={contractManager} setContractManager={setContractManager}/>
       </section>
     </Page>
