be59dbddeaa51e2e386f1ea7d9f4f324c2e5c9fa
Samuel Moor-Smith <samuelmoor-smith@Samuels-MacBook-Pro.local>
2023-06-30 21:15:18 -0700

    play with contract and aws

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,1 +1,4 @@
-export const contractAddr = '0xe7db0c69f359d5c18712ba7674ca4bd32c61bdba';
+export const contractAddr = '0xb4E31dFe8a7cDB7ece7361815a355dD459c5DfcB';
+export const COVALENT_API_URL = 'https://api.covalenthq.com/v1';
+export const COVALENT_API_KEY = 'cqt_rQRdgQcHRhB3XXkFH3T3wGxFMTwp'; // Replace with your API key
+export const CHAIN_ID = 5; 
